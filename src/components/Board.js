5d706adbf2db2124516f89aec770599ee468fabb
cwylycode <joeisnomoe@mail.com>
2022-03-16 12:13:35 -0600

    Used useRef for gameover

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,28 +6,29 @@ export default function Board({ grid, gridSize, cellClicked }) {
   const [board, setBoard] = useState([])
 
   useEffect(() => {
-    setBoard(updateBoard())
+    // Board gets recalculated everytime the grid changes. Whether this is efficient or not...I dunno lol.
+    setBoard(() => {
+      // console.log("updating board")
+      const arr = []
+      let i = 0
+      for (let y = 0; y < gridSize; y++) {
+        for (let x = 0; x < gridSize; x++) {
+          let borders = ""
+          if (y === gridSize - 1) {
+            if (x === gridSize - 1) borders = "border-top-0 border-end-0 border-bottom-0 border-start-0"
+            else borders = "border-top-0 border-bottom-0 border-start-0"
+          }
+          else if (x === gridSize - 1) borders = "border-top-0 border-end-0 border-start-0"
+          else borders = "border-top-0 border-start-0"
+          arr.push(<Cell key={i} cellID={i} cellValue={grid[i]} borders={borders} onClick={cellClicked} />)
+          i++
+        }
+      }
+      return arr
+    })
   }, [grid])
 
-  function updateBoard() {
-    console.log("updating board")
-    const arr = []
-    let i = 0
-    for (let y = 0; y < gridSize; y++) {
-      for (let x = 0; x < gridSize; x++) {
-        let borders = ""
-        if (y === gridSize - 1) {
-          if (x === gridSize - 1) borders = "border-top-0 border-end-0 border-bottom-0 border-start-0"
-          else borders = "border-top-0 border-bottom-0 border-start-0"
-        }
-        else if (x === gridSize - 1) borders = "border-top-0 border-end-0 border-start-0"
-        else borders = "border-top-0 border-start-0"
-        arr.push(<Cell key={i} cellID={i} cellValue={grid[i]} borders={borders} onClick={cellClicked} />)
-        i++
-      }
-    }
-    return arr
-  }
+
 
   return (
     <div className="container board ratio ratio-1x1 g-0">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./index.css"
-import {
-  TOKENS
-} from "./lib/constants.js"
+import { TOKENS, LINES } from "./lib/constants.js"
 
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
 import Board from './components/Board'
@@ -11,14 +9,66 @@ import History from './components/History'
 
 function App() {
 
+  const [grid, setGrid] = useState([null]);
   const [gridSize, setGridSize] = useState(3)
-  const [grid, setGrid] = useState(Array(gridSize * gridSize).fill(null));
-  const [userToken, setUserToken] = useState(TOKENS.X)
+  const [winningLines, setWinningLines] = useState([])
+  const [gameStatus, setGameStatus] = useState(null)
+  const gameOver = useRef(false)
+  // const [gameOver, setGameOver] = useState(false)
+  const [playersTurn, setPlayersTurn] = useState(true)
+  const [playerToken, setPlayerToken] = useState(TOKENS.X)
+  const [cpuToken, setCpuToken] = useState(TOKENS.O)
 
+  // Run at start and whenever a game-breaking setting is changed by user
+  useEffect(() => {
+    resetGame(true)
+  }, [gridSize, playerToken])
+
+  useEffect(() => {
+    const arr = Array(gridSize)
+    for (let line of winningLines) {
+      for (let i = 0; i < gridSize; i++) arr[i] = grid[line[i]]
+      // Winner declared, return token
+      if (arr.every(function (e) { return e === arr[0] })) setGameStatus(arr[0])
+    }
+    // All cells filled with no winner
+    if (grid.every(function (e) { return e !== null })) setGameStatus("draw")
+  }, [grid])
+
+  useEffect(() => {
+    if (gameStatus) {
+      if (gameStatus === "draw") console.log("Draw")
+      else console.log(`Winner: ${gameStatus}`)
+      gameOver.current = true
+    }
+  }, [gameStatus])
+
+  console.log(gameOver.current)
+
+  // Reset board and history - user override means user goes first, even if not their turn
+  function resetGame(userOverride = false) {
+    setPlayersTurn(userOverride ? true : playersTurn)
+    setGrid(Array(gridSize * gridSize).fill(null))
+    setWinningLines(LINES[gridSize - 3])
+    // todo: history erase
+    gameOver.current = false
+    // setGameOver(false)
+    console.log("reset")
+  }
+
+  // Both the player and computer will call this to place their tokens on the board
   function cellClicked(cellID) {
+    console.log(`Cell ${cellID} clicked`)
+    if (gameOver.current) {
+      resetGame()
+      return
+    }
+    if (grid[cellID]) return
+    const currentToken = playersTurn ? playerToken : cpuToken
     setGrid(prev => prev.map((value, index) => {
-      return index === cellID ? userToken : value
+      return index === cellID ? currentToken : value
     }))
+    setPlayersTurn(!playersTurn)
   }
 
   return (
