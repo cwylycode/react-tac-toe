4a8cefbb22fde86c0b669bb4aaf60bdf2d950bbb
cwylycode <joeisnomoe@mail.com>
2022-03-20 17:04:38 -0600

    Refactored board and app implementations

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,40 @@
-import { useEffect, useState } from "react"
+import { LINES } from "../lib/constants.js"
 import Cell from "./Cell"
 
-export default function Board({ grid, gridSize, cellClicked }) {
+import { useEffect, useState } from "react"
 
-  const [board, setBoard] = useState([])
+export default function Board({ gridSize, onGameOver, playerToken, cpuToken, history }) {
+
+  const [boardValues, setBoardValues] = useState(null)
+  const [grid, setGrid] = useState([])
+  const [winningLines, setWinningLines] = useState([])
+  const [playersTurn, setPlayersTurn] = useState(true)
+  const [status, setStatus] = useState(null)
 
   useEffect(() => {
-    // Board gets recalculated everytime the grid changes. Whether this is efficient or not...I dunno lol.
-    setBoard(() => {
-      // console.log("updating board")
+    reset(true) // Manually reset board if game-breaking settings change
+  }, [gridSize, playerToken])
+
+  useEffect(() => {
+    // Prevent rendering the grid twice on startup
+    if (boardValues) updateGrid()
+    if (status) onGameOver(status)
+  }, [boardValues, status]) // Status needed for cell-click reset to work
+
+  useEffect(() => {
+    if (!boardValues) return
+    const arr = Array(gridSize)
+    for (let line of winningLines) {
+      for (let i = 0; i < gridSize; i++) arr[i] = boardValues[line[i]]
+      // Winner declared, return token
+      if (arr.every(function (e) { return e === arr[0] })) setStatus(arr[0])
+    }
+    // All cells filled with no winner
+    if (boardValues.every(function (e) { return e !== null })) setStatus("draw")
+  }, [grid])
+
+  function updateGrid() {
+    setGrid(() => {
       const arr = []
       let i = 0
       for (let y = 0; y < gridSize; y++) {
@@ -20,20 +46,40 @@ export default function Board({ grid, gridSize, cellClicked }) {
           }
           else if (x === gridSize - 1) borders = "border-top-0 border-end-0 border-start-0"
           else borders = "border-top-0 border-start-0"
-          arr.push(<Cell key={i} cellID={i} cellValue={grid[i]} borders={borders} onClick={cellClicked} />)
+          arr.push(<Cell key={i} cellID={i} cellValue={boardValues[i]} borders={borders} handleClick={handleCellClick} />)
           i++
         }
       }
       return arr
     })
-  }, [grid])
+  }
 
+  // Reset board and history - user override means user goes first, even if not their turn
+  function reset(userOverride = false) {
+    setBoardValues(Array(gridSize * gridSize).fill(null))
+    setWinningLines(LINES[gridSize - 3])
+    setStatus(null)
+    setPlayersTurn(userOverride ? true : playersTurn)
+  }
 
+  // Both the player and computer will call this to place their tokens on the board
+  function handleCellClick(cellID) {
+    if (status) {
+      reset()
+      return
+    }
+    if (boardValues[cellID]) return
+    const currentToken = playersTurn ? playerToken : cpuToken
+    setBoardValues(prev => prev.map((val, idx) => {
+      return idx === cellID ? currentToken : val
+    }))
+    setPlayersTurn(!playersTurn)
+  }
 
   return (
     <div className="container board ratio ratio-1x1 g-0">
-      <div className={`grid row row-cols-${gridSize} g-0`}>
-        {board}
+      <div className={`row row-cols-${gridSize} g-0`}>
+        {grid}
       </div>
     </div>
   )
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,18 +4,20 @@ import { TOKENS } from "../lib/constants.js"
 
 import { useEffect, useState } from "react"
 
-export default function Cell({ cellID, cellValue, borders, onClick }) {
+export default function Cell({ cellID, cellValue, borders, handleClick }) {
   const [icon, setIcon] = useState(null)
+  const [animation, setAnimation] = useState("")
 
   useEffect(() => {
+    setAnimation(cellValue ? "zoomIn" : "zoomOut")
     if (cellValue === TOKENS.X) setIcon(iconX)
     else if (cellValue === TOKENS.O) setIcon(iconO)
     else setIcon("")
   }, [cellValue])
 
   return (
-    <button id={cellID} type="button" onClick={() => { onClick(cellID) }} className={`cell col btn position-relative rounded-0 border-dark border-5 ${borders}`}>
-      <img className="cell-icon position-absolute translate-middle" src={icon}></img>
+    <button id={cellID} type="button" onClick={() => (handleClick(cellID))} className={`cell col btn position-relative rounded-0 border-dark border-5 ${borders}`}>
+      <img className={`cell-icon position-absolute translate-middle ${animation}`} src={icon}></img>
     </button>
   )
 }
