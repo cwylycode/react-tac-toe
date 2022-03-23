55e79bf1621c8b6bd8569bd4997aabe9688af948
cwylycode <joeisnomoe@mail.com>
2022-03-23 12:18:19 -0600

    Wrote storage functionality

---
+++ src/lib/storage.js
@@ -0,0 +1,19 @@
+export default function getData(key) {
+  if (!localStorage) return;
+
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.error(`Error getting item ${key} from localStorage`, err);
+  }
+};
+
+export default function setData(key, item) {
+  if (!localStorage) return;
+
+  try {
+    return localStorage.setItem(key, JSON.stringify(item));
+  } catch (err) {
+    console.error(`Error storing item ${key} to localStorage`, err);
+  }
+};
