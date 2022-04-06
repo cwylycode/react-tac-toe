55bdb774102106f5ab1d23af03bf467b7131ab2f
cwylycode <joeisnomoe@mail.com>
2022-04-05 19:13:26 -0600

    Fixed referrer

--- src/components/ModalInfo.js
+++ src/components/ModalInfo.js
@@ -3,7 +3,7 @@ import { BsGithub } from "react-icons/bs"
 
 export default function ModalInfo() {
   const contextButtons = <>
-    <a href="https://github.com/cwylycode/react-tac-toe" target={"_blank"} className="btn btn-bs btn-success">
+    <a href="https://github.com/cwylycode/react-tac-toe" target={"_blank"} referrerPolicy={"noreferrer"} className="btn btn-bs btn-success">
       <div className="d-flex justify-content-center align-items-center">
         <BsGithub />
         <p className="m-0 ms-2">See on GitHub</p>
@@ -17,7 +17,7 @@ export default function ModalInfo() {
         <p>A Tic-Tac-Toe game made with React</p>
         <p>Pick your token and play against the computer and win (or lose...or draw). But most of all, have fun!</p>
         <br />
-        <p>Made by <a href="https://cwylycode.github.io" target={"_blank"} referrerPolicy={"no-referrer"}>Colin Wyly</a></p>
+        <p>Made by <a href="https://cwylycode.github.io" target={"_blank"} referrerPolicy={"noreferrer"}>Colin Wyly</a></p>
         <p>This web app is free and open source. For more information about the app and project, vist the GitHub link below.</p>
         <p>Feel free to check out my other projects, too!</p>
       </div>
